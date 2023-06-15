7b0cf3512a502147c45c3f6fb0882344507f4f55
mostafizul <mostafizulkarim3395@gmail.com>
2023-06-16 01:20:50 +0600

    second push

--- practice.js
+++ practice.js
@@ -1,6 +1,38 @@
+/*
 var name = 'siam';
 var number = 12;
 var boolean = !true
 console.log(name);
 console.log(typeof number);
-console.log(boolean);
+console.log(boolean);*/
+
+/*
+var number1 = 2.55555555;
+var number2 = 'siam'
+console.log(number1.toFixed(3));
+console.log(number1.toPrecision(3));
+console.log(typeof toString(number1));
+console.log(typeof Number(number2));*/
+
+/*
+var firstName = "Md."
+var lastName = "Karim"
+console.log(firstName + " " + lastName);*/
+
+/*
+var firstName = prompt("Enter a string: ");
+
+console.log(firstName.length);
+console.log(firstName.toUpperCase());
+console.log(firstName.charAt(3));
+console.log(firstName.slice(3,5));
+*/
+/*
+var firstName = prompt("Enter firstName: ");
+var lastName = prompt("Enter lastName: ");
+var fullName = firstName+ " " + lastName;
+console.log(fullName);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+console.log(fullName.charAt(2));
+*/
